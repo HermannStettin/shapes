18b00b03f1c2cdc309358451f1eb867e7f5d1508
HermannStettin <chuvashpeople@yandex.ru>
2025-06-10 19:29:59 +0300

    fix: join paths and move assignment to device outside

--- data.py
+++ data.py
@@ -1,5 +1,6 @@
 from torchvision import transforms, datasets
 from torch.utils.data import DataLoader
+import os
 
 def get_train_val_test_data(data_params, batch_size, device):
     train_transforms = transforms.Compose([
@@ -23,21 +24,16 @@ def get_train_val_test_data(data_params, batch_size, device):
                             [0.229, 0.224, 0.225])
     ])
 
-    train_path = data_params["data_path"] / "train"
-    test_path = data_params["data_path"] / "test"
-    val_path = data_params["data_path"] / "val"
+    train_path = os.path.join(data_params["data_path"], "train")
+    test_path = os.path.join(data_params["data_path"], "test")
+    val_path = os.path.join(data_params["data_path"], "val")
 
     train_data = datasets.ImageFolder(root = train_path, transform = train_transforms)
     val_data = datasets.ImageFolder(root = val_path, transform = val_transforms)
     test_data = datasets.ImageFolder(root = test_path, transform = test_transforms)
-
-    train_data = train_data.to(device)
-    val_data = val_data.to(device)
-    test_data = test_data.to(device)
 
     train_loader = DataLoader(dataset = train_data, batch_size = batch_size, shuffle = True)
     test_loader = DataLoader(dataset = test_data, batch_size = batch_size, shuffle = False)
     val_loader = DataLoader(dataset = val_data, batch_size = batch_size, shuffle = False)
 
     return train_loader, test_loader, val_loader
-
